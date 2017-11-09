ea508d8f0e116825f5eb1704c9dd27a295995503
John <jjcerpa@uninorte.edu.co>
2017-11-08 21:11:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -221,7 +221,7 @@ function collectCoin(mario, coin) {
 }
 
 function launchBarrels() {
-	for (var i = 1; i <= 4; i++) {
+	for (var i = 1; i <= 0; i++) {
 		var position = Math.random() * (game.width - platformWidth - 60);
 		var barrel = barrels.create(position, -10, 'barrel');
 		barrel.body.gravity.y = 300;
